dfd382e2d56d780905e09883ef86ddfcf3b074b9
ciddarum <cudarum@up.edu.ph>
2016-11-14 03:43:02 -0800

    Fixed login page and added index page

--- test.py
+++ test.py
@@ -1,36 +1,67 @@
 import pymysql as mariadb
 import Tkinter as tk
 
-def loginClick(user, passw, cursor):
+def loginClick(user, passw, cursor, index):
     try:
         cursor.execute("SELECT * FROM USER WHERE username = %s AND password = password(%s)"
                         ,(user, passw))
     except:
         print "Error in selection"
         
-    print user, passw
-    for result in cursor:
-        print result
+    if cursor.rowcount == 1:
+        print "Change to index"
+        raise_frame(index)
 
 def initializeWindow():
-    #window
     frame = tk.Tk()
     frame.title("Testing")
     frame.geometry("600x400")
+    frame.maxsize(width = 600, height = 400)
+    frame.minsize(width = 600, height = 400)
+    frame.config(bg = "#CBAA7B")
     
     return frame
     
+def initializeConnection(user, password, database):
+    return mariadb.connect( user = user,
+                            password = password,
+                            database = database
+    )
+
+def createLogin(frame, cursor, frames):
+    #textboxes
+    usernamebox = tk.Entry(frame)
+    passwordbox = tk.Entry(frame, show = "*")
+    
+    #nested helper
+    def loginHelper():
+        loginClick(usernamebox.get(), passwordbox.get(), cursor, frames["index"])
+
+    #login button
+    login = tk.Button(frame, text = "Login", command = loginHelper)
+    #usernameLabel = tk.Label(window, text = "Username: ")
+    #passwordLabel = tk.Label(window, text = "Password: ")
+
+    #applying layout manager to elements
+    usernamebox.pack(pady = (100,10))
+    passwordbox.pack(pady = (0,10))
+    login.pack()
+
+def createIndex(frame, cursor, frames):
+    logout = tk.Button( 
+        frame, 
+        text = "Logout", 
+        command = lambda:raise_frame(frames["login"])
+    )
+    logout.pack()
+
+#changes window
+def raise_frame(frame):
+    frame.tkraise()
 
 def main():
-    user = "ciddarum"
-    password = "admin"
-    database = "JobFinder"
     #initialization of connection
-    connection = mariadb.connect(
-        user = user, 
-        password = password,
-        database = database
-    )
+    connection = initializeConnection("ciddarum", "admin", "JobFinder")
 
     #initialization of cursor
     cursor = connection.cursor(mariadb.cursors.DictCursor)
@@ -38,26 +69,21 @@ def main():
     #initialization of window
     window = initializeWindow()
     
-    #textboxes
-    usernamebox = tk.Entry(window)
-    passwordbox = tk.Entry(window, show = "*")
+    #initialization of frames
+    frames = {
+        "login":tk.Frame(window, bg = "#CBAA7B", width = 600, height = 400),
+        "index":tk.Frame(window, bg = "#CBAA7B", width = 600, height = 400)
+    }
     
-    #nested helper
-    def loginHelper():
-        loginClick(usernamebox.get(), passwordbox.get(), cursor)
-
-    #login button
-    login = tk.Button(window, text = "Login", command = loginHelper)
-
-    #gets frame of window
-    frame = tk.Frame(window)
+    #inserting frames into the main window
+    for frame in frames:
+        frames[frame].grid(row=0, column=0, sticky='news', padx = 225)
     
-    frame.grid()
-    usernamebox.grid()
-    passwordbox.grid()
-    login.grid()
-
+    #creating frames
+    createLogin(frames["login"], cursor, frames)
+    createIndex(frames["index"], cursor, frames)
     #starts loop of window
+    raise_frame(frames["login"])
     window.mainloop()
     #commits changes to the database
     connection.commit()
