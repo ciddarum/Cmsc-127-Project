db380c70e0010c657fe07c92a5fb94a59b51d15a
njisip <cyber2103@gmail.com>
2016-12-12 03:59:14 +0800

    Add datasets

--- schema.sql
+++ schema.sql
@@ -50,8 +50,7 @@ CREATE TABLE IF NOT EXISTS `COMPANYREP`(
 	`Privilege` varchar(100),
 	`Companyid` int(5) default NULL,
 	`Companyname` varchar(41),
-	
-	UNIQUE (Companyname),
+
 	FOREIGN KEY(Userid) REFERENCES USERS(Userid) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY(Companyid) REFERENCES COMPANY(Companyid) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -144,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `JOBSKILLSETREQ`(
 
 
 DELIMITER %%
--------------TRIGGERS----------------
+#-------------TRIGGERS----------------
 	CREATE TRIGGER userInsert AFTER INSERT ON USERS
 		FOR EACH ROW
 			BEGIN
@@ -184,7 +183,7 @@ DELIMITER %%
 				INSERT INTO activityLog(activity, OldValue) VALUES("Deleted a Company", OLD.Companyname);
 			END;
 %%
--------------PROCEDURES-------------------
+#-------------PROCEDURES-------------------
 	CREATE PROCEDURE userInsertLog(in uname varchar(25), in pword varchar(41), in userName varchar(40))
 		BEGIN
 			INSERT INTO USERS(Username, Password, Name) VALUES(uname, md5(pword), userName);
@@ -208,7 +207,7 @@ DELIMITER %%
 		END;
 
 %%
-----CREATE USER(JOBSEEKER/COMPANY REP)------
+#----CREATE USER(JOBSEEKER/COMPANY REP)------
 	CREATE PROCEDURE jsInsertLog(in jsAge int(2))
 		BEGIN	
 			INSERT INTO JOBSEEKER(Userid, Age) VALUES((select Userid from USERS where Userid = LAST_INSERT_ID()), jsAge);
@@ -237,7 +236,7 @@ DELIMITER %%
 		END;
 
 %%
----DELETE JOBSEEKER----
+#---DELETE JOBSEEKER----
 	CREATE PROCEDURE jsDeleteLog(in Usid int(5))
 		BEGIN
 			
@@ -251,7 +250,7 @@ DELIMITER %%
 		END;
 		
 %%
-----UPDATE JOBSEEKER--------
+#----UPDATE JOBSEEKER--------
 	CREATE PROCEDURE jsUpdateLog(in Usid int(5), in uname varchar(25), in pword varchar(41), in jsName varchar(40), in jsAge int(2))
 		BEGIN
 			INSERT INTO activityLog(activity, OldValue, NewValue) VALUES(concat("Updated User: ", uname),concat((select Username from USERS where Userid = Usid), "-", (select Password from USERS where Userid = Usid), "-", (select Name from USERS where Userid = Usid), "-", (select Age From JOBSEEKER where Userid = Usid)), concat(uname, "-", md5(pword), "-",jsName, "-",jsAge));
@@ -260,9 +259,9 @@ DELIMITER %%
 			UPDATE JOBSEEKER SET Age = jsAge where Userid = Usid;
 		END;
 %%
------------------------------------------------------------
------------------------------------------------------------
-------------COMPANY REP-------------
+#-----------------------------------------------------------
+#-----------------------------------------------------------
+#------------COMPANY REP-------------
 	CREATE PROCEDURE cInsertLog(in cPrivilege varchar(100), in cCompanyname varchar(41))
 		BEGIN
 			INSERT INTO COMPANYREP(Userid, Privilege, Companyid, Companyname) VALUES((select Userid from USERS where Userid = LAST_INSERT_ID()), cPrivilege, (select Companyid from COMPANY where Companyname = cCompanyname), cCompanyname);
@@ -270,7 +269,7 @@ DELIMITER %%
 		END;
 %%
 
-----DELETE COMPANY REP -----
+#----DELETE COMPANY REP -----
 	CREATE PROCEDURE cDeleteLog(in Usid int(5))
 		BEGIN
 			DELETE FROM USERS where Userid = Usid;
@@ -281,7 +280,7 @@ DELIMITER %%
 		END;
 		
 %%
------UPDATE COMPANY REP-------
+#-----UPDATE COMPANY REP-------
 	CREATE PROCEDURE cUpdateLog(in Usid int(5), in uname varchar(25), in pword varchar(41), in cName varchar(40), in cPrivilege varchar(100), in cCompanyname varchar(41))
 		BEGIN
 			INSERT INTO activityLog(activity, OldValue, NewValue) VALUES(concat("Updated Company Representative: ", (select Username from USERS where Userid = Usid)), concat((select Username from USERS where Userid = Usid), "-", (select Password from USERS where Userid = Usid), "-", (select Name from USERS where Userid = Usid), "-", (select Privilege from COMPANYREP where Userid = Usid), "-", (select CompanyName from COMPANYREP where Userid = Usid)), concat(uname, "-",md5(pword), "-",cName, "-",cPrivilege, "-",cCompanyname));
@@ -292,7 +291,7 @@ DELIMITER %%
 		END;
 	
 		
-%% ---JOB ACTIVITY LOG---
+%% #---JOB ACTIVITY LOG---
 
 	CREATE PROCEDURE jobInsertLog(in ind varchar(50), in jTitle varchar(50), in areq int(2), in lev varchar(20), in sal varchar(50), in edate datetime, in status varchar(10), in uname varchar(25))
 		BEGIN
@@ -322,7 +321,7 @@ DELIMITER %%
 			UPDATE JOB SET Industry = ind, Jobtitle = jTitle, Agerequirement = areq, JOB.JLevel = lev, Salary = sal, Enddate = edate, Status = status where JOB.Jobid = job_id;
 		END;
 		
-%%----COMPANY LOG-----
+%%#----COMPANY LOG-----
 	CREATE PROCEDURE companyInsertLog(in cname varchar(41), in dtails varchar(25))
 		BEGIN
 			INSERT INTO COMPANY(Companyname, Details) VALUES(cname, dtails);
@@ -352,8 +351,8 @@ DELIMITER %%
 		END;
 
 %%
---------Print Procedure---------
---------Print JOBSEEKER---------
+#--------Print Procedure---------
+#--------Print JOBSEEKER---------
 	CREATE PROCEDURE jsPrint(in jsId int(5))
 		BEGIN
 			SELECT A.Userid, A.Username, A.Password, A.Name, B.ContactNumber, C.Emailaddress, JSA.Address, JSSS.Skillset, JSEA.EducationalAttainment From USERS AS A JOIN USERCONTACTNUMBER AS B JOIN USEREMAILADDRESS AS C JOIN JOBSEEKERADDRESS AS JSA JOIN JOBSEEKERSKILLSET AS JSSS JOIN JSEDUCATIONALATTAINMENT AS JSEA ON (A.Userid = jsId) and A.Userid = B.Userid and (A.Userid = C.Userid) and A.Userid = JSA.Userid and A.Userid = JSSS.Userid and A.Userid = JSEA.Userid;
@@ -365,7 +364,7 @@ DELIMITER %%
 		END;
 		
 %%
-------COMPANY REP-------
+#------COMPANY REP-------
 	CREATE PROCEDURE compRepPrint(in cid int(5))
 		BEGIN
 			SELECT A.Userid, A.Username, A.Password, A.Name, B.ContactNumber, C.Emailaddress, CREP.Privilege, CREP.Companyid, CREP.Companyname From USERS AS A JOIN USERCONTACTNUMBER AS B JOIN USEREMAILADDRESS AS C JOIN COMPANYREP AS CREP ON (A.Userid = cid) and A.Userid = B.Userid and (A.Userid = C.Userid) and A.Userid = CREP.Userid;
@@ -378,7 +377,7 @@ DELIMITER %%
 		END;
 		
 %%
-------COMPANY----------
+#------COMPANY----------
 	CREATE PROCEDURE cPrint(in compId int(5))
 		BEGIN
 			SELECT A.Companyid, A.Companyname, A.Details, B.Address FROM COMPANY AS A JOIN COMPANYADDRESS AS B ON (A.Companyid = compId) and A.Companyid = B.Companyid;
